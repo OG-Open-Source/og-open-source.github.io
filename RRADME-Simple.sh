f58b0a4d8943d179b9f166e656bcf9bf23f164f6
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-08-29 14:18:46 +0800

    Create RRADME-Simple.sh

---
+++ RRADME-Simple.sh
@@ -0,0 +1,33 @@
+
+<!-- You can only change the [Year] and [Original Author Name] in the copyright statement. -->
+
+# OG|OS - OGATA Open Source
+
+## License
+
+<!-- This repository is licensed under the [Apache License](https://www.apache.org/licenses). -->
+<!-- This repository is licensed under the [BSD License](https://opensource.org/licenses/bsd-license.php). -->
+<!-- This repository is licensed under the [MIT License](https://opensource.org/licenses/mit-license.php). -->
+<!-- This repository is licensed under the [GPL License](https://www.gnu.org/licenses/gpl-3.0.html). -->
+
+This repository was added with the permission of the original author.
+
+© [Year] [Original Author Name](Original.Author.Repositorie). All rights reserved.
+
+---
+
+## Changelog
+
+### [1.0.0] - 2024-08-28
+
+- Initial release of the project
+
+---
+
+## Introduction
+
+OGOS - OGATA Open Source is dedicated to developing and sharing open-source software. Our goal is to create and maintain high-quality, community-driven projects that benefit developers and users worldwide.
+
+## Disclaimer
+
+The software in this repository is provided "as-is," without warranty of any kind, express or implied. The use of the software is at your own risk.
